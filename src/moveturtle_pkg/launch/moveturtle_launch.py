83f235e5580f35eb1bad859b85cb10ce51a629ac
Valeria <valeriavallebueno@gmail.com>
2025-09-17 17:10:12 +0200

    Updated moveturtle_launch.py

---
+++ src/moveturtle_pkg/launch/moveturtle_launch.py
@@ -0,0 +1,14 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='moveturtle_pkg',
+            executable='moveturtle_exec',
+            output='screen'),
+        Node(
+            package='turtlesim',
+            executable='turtlesim_node',
+            output='screen'),
+    ])
